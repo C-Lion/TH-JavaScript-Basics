91bc11e799a739346b7fe42bdb263b1d11a94451
C-Lion <esstepansky@gmail.com>
2018-09-23 15:43:40 +0300

    add js file for combiningStrings

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-alert("Hey, Its time for more JS!");s
+//Now I know 3 different ways to output messages:
+// 1. Dialogue box or alert (pop up) messages:
+alert("Hey, Its time for more JS!");
+
+//Write text on the HTML page:
+document.write("Now it works!");
+
+//Prints messages in the console: 
+//very hand for troubleshooting. 
+//Just make sure the console is open to see it!
+//cmd+shift+j
+console.log("Program Completed");
